3533c9c4e6bbe180bd288e811fa8562f92bf476b
tuguzT <timurka.tugushev@gmail.com>
2022-09-10 21:48:43 +0300

    Add the simplest endpoint for framework testing

--- flexible-project/src/main.rs
+++ flexible-project/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![warn(missing_docs)]
+#![warn(clippy::all)]
+
+//! Flexible Project server.
+
+use actix_web::{get, App, HttpServer, Responder};
+
+/// The most simple HTTP GET endpoint.
+#[get("/")]
+async fn hello() -> impl Responder {
+    "Hello world!"
+}
+
+/// Entry point of the server.
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
