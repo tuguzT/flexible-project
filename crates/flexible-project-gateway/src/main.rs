ab0a265abca0531351c488af818a1c1ab1f7a530
tuguzT <timurka.tugushev@gmail.com>
2023-05-13 15:10:05 +0300

    Add empty server implementation with tracing support

--- crates/flexible-project-gateway/src/main.rs
+++ crates/flexible-project-gateway/src/main.rs
@@ -4,6 +4,34 @@
 #![warn(clippy::all)]
 #![forbid(unsafe_code)]
 
-fn main() {
-    todo!()
+use anyhow::{Context, Result};
+use axum::{Router, Server};
+use std::net::SocketAddr;
+use tower_http::trace::TraceLayer;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .try_init()
+        .with_context(|| "failed to init tracing subscriber")?;
+
+    let app = Router::new().layer(TraceLayer::new_for_http());
+
+    let addr = &SocketAddr::from(([0, 0, 0, 0], 8080));
+    tracing::info!("listening on {}", addr);
+    Server::bind(addr)
+        .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .with_context(|| "failed to init server")?;
+    Ok(())
+}
+
+/// Catches a signal to shut down the server.
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("expect tokio signal ctrl-c")
 }
